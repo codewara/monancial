b5e09b6a48637e3faa8e85a9c7674cac0d3e55c6
Daneswara <150428419+codewara@users.noreply.github.com>
2024-12-26 00:20:21 +0700

    simple footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center py-5 text-white bg-[var(--background)]">
+            <p className="text-center">
+                &copy; {new Date().getFullYear()} monancial. All rights reserved.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
