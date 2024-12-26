731c49a2b0bd031be1e7064b2da78d281b7e6b99
Daneswara <150428419+codewara@users.noreply.github.com>
2024-12-26 15:55:46 +0700

    fixed carousel

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect, useCallback } from 'react';
+import { useSwipeable } from 'react-swipeable';
 import Image from 'next/image';
 
 interface Slide {
@@ -14,6 +15,7 @@ interface CarouselProps {
 
 const Carousel = ({ slides } : CarouselProps) => {
     const [current, setCurrent] = useState(0);
+    const screenMD = window.innerWidth >= 768;
 
     const nextSlide = useCallback(() => {
         setCurrent((current) => (current === slides.length - 1 ? 0 : current + 1));
@@ -23,46 +25,54 @@ const Carousel = ({ slides } : CarouselProps) => {
         setCurrent((current) => (current === 0 ? slides.length - 1 : current - 1));
     }, [slides.length]);
     
+    const swipeHandlers = useSwipeable({
+        onSwipedLeft: nextSlide,
+        onSwipedRight: prevSlide,
+        preventScrollOnSwipe: true,
+        trackMouse: true,
+    });
+    
     useEffect(() => {
         const interval = setInterval(nextSlide, 5000);
         return () => clearInterval(interval);
     }, [nextSlide]);
 
     return (
-        <div className="relative w-full h-full overflow-hidden">
+        <div {...swipeHandlers} className={`relative w-full h-full overflow-hidden`}>
             {/* Slides Container */}
             <div
-                className="relative h-full flex transition-transform duration-700 ease-in-out"
+                className={`relative h-full flex transition-transform duration-700 ease-in-out`}
                 style={{ transform: `translateX(-${current * 100}%)` }}
             >
                 {slides.map((slide) => (
-                    <div key={slide.id} className="relative w-full h-full flex-shrink-0">
+                    <div key={slide.id} className={`relative w-full h-full flex-shrink-0`}>
                         <Image
                             src={slide.url}
                             alt={slide.alt}
-                            layout="fill"
-                            objectFit="cover"
+                            layout={`fill`}
+                            objectFit={`cover`}
                         />
                     </div>
                 ))}
             </div>
+
             {/* Buttons */}
             <button
-                className="absolute top-0 left-0 w-[15%] h-full"
+                className={`top-0 left-0 w-[15%] h-full bg-black ${screenMD ? `absolute` : `hidden`}`}
                 onClick={prevSlide}
             ></button>
             <button
-                className="absolute top-0 right-0 w-[15%] h-full"
+                className={`top-0 right-0 w-[15%] h-full bg-black ${screenMD ? `absolute` : `hidden`}`}
                 onClick={nextSlide}
             ></button>
 
-            {/* Indicators */}
-            <div className=" absolute bottom-0 left-1/2 space-x-3 transform -translate-y-1/2 -translate-x-1/2">
+            {/* Indicator */}
+            <div className={`absolute bottom-0 left-1/2 space-x-3 transform -translate-y-1/2 -translate-x-1/2`}>
                 {slides.map((_, idx) => (
                 <button
                     key={idx}
                     className={`w-2 h-2 rounded-full ${
-                        idx === current ? 'bg-white' : 'bg-gray-500'
+                        idx === current ? `bg-white` : `bg-gray-500`
                     }`}
                     onClick={() => setCurrent(idx)}
                 />
